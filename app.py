368b2812f6f8ccec0f52fe6ba3d19f6a1cdb484f
Kimws0807 <64004470+Kimws0807@users.noreply.github.com>
2020-04-26 21:23:48 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -10,61 +10,56 @@ members = [
 ]
 
 def get_menu():
-    menu_temp = "<li><a href='/{0}'>{0}</a></li>"
-    menu = [e for e in os.listdir('menu') if e[0] != '.']
+    menu_temp = "<li><a href='{0}'>{0}</a></li>"
+    menu = [e for e in os.listdir('views') if e[0] != '.']
     return "\n".join([menu_temp.format(m) for m in menu])
 
 def get_template(filename):
-    with open('views/' + filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         template = f.read()
         
     return template
 
-def get_page(filename):
-    with open(f'menu/{filename}.html', 'r', encoding="utf-8") as f:
-        template = f.read()
+# def get_page(filename):
+#     with open(f'menu/{filename}.html', 'r', encoding="utf-8") as f:
+#         template = f.read()
         
-    return template
+#     return template
 
 @app.route("/")
 def index():
-    id = request.args.get('id', '')
-    template = get_template('template.html')
-    
-    title = 'Welcome ' + id + '씨'
-    content = """환영합니다."""
-#             이곳에서 당신의 주요사항을 기록하며 관리하실 수 있습니다.'
-    menu = get_menu()
-    return template.format(title, content, menu)
+    with open('template.html', 'r', encoding="utf-8") as f:
+        default = f.read()
+    return default
 
 
 @app.route("/<title>")
 def html(title):
-    template = get_page(title)
+    template = get_template(title)
     menu = get_menu()
-        
+
     if title not in menu:
         return abort(404)
-        
-    with open(f'menu/{title}.html', 'r', encoding="utf-8") as f:
+
+    with open(f'views/{title}.html', 'r', encoding="utf-8") as f:
         content = f.read()
-            
+
     return template.format(title, content, menu)
 
 
-@app.route("/create", methods=['GET', 'POST'])
-def create():
-    template = get_template('create.html')
-    menu = get_menu()
+# @app.route("/create", methods=['GET', 'POST'])
+# def create():
+#     template = get_template('create.html')
+#     menu = get_menu()
     
-    if request.method == 'GET':
-        return template.format('', menu)
+#     if request.method == 'GET':
+#         return template.format('', menu)
     
-    elif request.method == 'POST':
-        with open(f'content/{request.form["title"]}', 'w') as f:
-            f.write(request.form['desc'])
+#     elif request.method == 'POST':
+#         with open(f'/{request.form["title"]}', 'w') as f:
+#             f.write(request.form['desc'])
             
-        return redirect('/')
+#         return redirect('/')
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -85,18 +80,36 @@ def login():
             return template.format("<p>패스워드를 확인해 주세요</p>", menu)
         
         # 검증 성공하여 로그인
-        return redirect("/main?id=" + m[0]['id'])
+        return redirect("views/Home?id=" + m[0]['id'])
 
-@app.route("/main")
+@app.route("/views/Home")
 def main():
     id = request.args.get('id', '')
-    template = get_template('main.html')
+    template = get_template('views/Home')
     
     title = 'Welcome ' + id + '씨'
     content = """환영합니다."""
-#             이곳에서 당신의 주요사항을 기록하며 관리하실 수 있습니다.'
     menu = get_menu()
     return template.format(title, content, menu)
+
+
+@app.route("/views/<title>" , methods=['GET', 'POST'])
+def html2(title):
+    id = request.args.get('id', '')
+    template = get_template('template2.html')
+    menu = get_menu()
+
+    with open(f'views/{title}', 'r', encoding="utf-8") as f:
+        content = f.read()
+    
+    if request.method == 'GET':
+        return template.format(id, content, menu)
+
+    elif request.method == 'POST':
+        with open(f'database/{request.form["subject"]}', 'w', encoding="utf-8") as f:
+            f.write(request.form['contents'])
+  
+    return redirect(f'{title}')
 
 
 @app.route('/favicon.ico')
